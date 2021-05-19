21e2bb807ce41334a1c3df09d432ad98cd69b020
Rachit Garg <rachitgarg900@gmail.com>
2021-05-19 02:31:08 +0200

    added connection test example

---
+++ ConnectionTest/ConnectionTest.py
@@ -0,0 +1,55 @@
+#!python3
+
+'''
+    code referred from http://www.steves-internet-guide.com/client-connections-python-mqtt/
+    need to run mosquitto -c <path to conf file>
+    'listner 1883' and 'allow_anonymous true' fields need to be set in conf file
+'''
+
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+
+    if rc == 0 :
+        print("Successfully connected!")
+        client.is_connected = True
+
+    else:
+        print("Error connecting:",rc)
+
+
+broker = '192.168.0.109'
+port = '' #not necessary as it is default to 1883, if different then change it and pass it to connect() function
+
+# set the is_connected flag as a variable in Client class
+mqtt.Client.is_connected = False
+
+client = mqtt.Client("PythonTest") #create Paho client object
+client.on_connect = on_connect #bind the on_connect function as the callback for client object
+
+try:
+    client.connect(broker) #connect to broker
+    print('Connecting to broker', broker)
+
+    client.loop_start() #start network loop
+
+    while client.is_connected == False: #Wait loop
+        print('In wait loop')
+        time.sleep(1)
+
+    print('In main loop')
+
+    client.loop_stop() #stop network loop
+    client.disconnect() #disconnect from broker
+
+except:
+    print('Connection refused!')
+    exit(1)
+
+
+
+
+
+
+
